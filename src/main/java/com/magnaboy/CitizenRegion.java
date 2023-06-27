7a1eccbc09b5bc09d1e044810a16ec56399ec068
Nathan Dugan <nrdugan2010@gmail.com>
2023-06-27 09:37:44 -0400

    Scriptable Citizens via JSON.

--- src/main/java/com/magnaboy/CitizenRegion.java
+++ src/main/java/com/magnaboy/CitizenRegion.java
@@ -2,6 +2,9 @@ package com.magnaboy;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.magnaboy.serialization.CitizenInfo;
+import com.magnaboy.serialization.EntityInfo;
+import com.magnaboy.serialization.SceneryInfo;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -157,8 +160,10 @@ public class CitizenRegion {
 	}
 
 	private static ScriptedCitizen loadScriptedCitizen(CitizensPlugin plugin, CitizenInfo info) {
-		info.entityType = EntityType.StationaryCitizen;
-		return new ScriptedCitizen(plugin).setWorldLocation(info.worldLocation);
+		info.entityType = EntityType.ScriptedCitizen;
+		return new ScriptedCitizen(plugin)
+			.setWorldLocation(info.worldLocation)
+			.setScript(info.startScript);
 	}
 
 	public static Scenery loadScenery(CitizensPlugin plugin, SceneryInfo info) {
---
+++ src/main/java/com/magnaboy/scripting/ActionType.java
@@ -0,0 +1,9 @@
+package com.magnaboy.scripting;
+
+public enum ActionType {
+	Idle,		//Do literally nothing. Can be used to pause between actions
+	WalkTo,		//Go somewhere
+	Animation,  //Sets idle animation, can be one-shot or remain as idle anim
+	Say,		//Overhead text message
+	StartNewScript,//Can start a completely new script from a script
+}
